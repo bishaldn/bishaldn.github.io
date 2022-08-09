1589d17711feaca80dcc8a0f500bf46ddbf321d6
bishaldn <bdn0298@gmail.com>
2022-08-09 23:16:05 +0530

    updated

--- js/index.js
+++ js/index.js
@@ -4,3 +4,17 @@ mobile_nav.addEventListener('click',() => toggleNavbar());
 function toggleNavbar(){
      show.classList.toggle("show");
 }
+$(document).ready(function () {
+     $(window).scroll(function () {
+         var pos = $(document).scrollTop();
+         console.log(pos);
+     //     if((pos>2)&& (pos<104)){
+     //         $('.card-1').addClass(' animate__animated animate__backInRight');
+     //         $('.card-2').addClass(' animate__animated animate__backInDown');
+     //         $('.card-3').addClass(' animate__animated animate__backInRight');
+     //     }
+     //     if(pos==177){
+     //         $('.text').addClass('animate__animated animate__animated animate__backInRight');
+     //     }
+     });
+ });
